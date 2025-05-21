7a20278755c1ad4ebf0c567f747ddf8d7a8f12be
Erick Dorta Cardenas <edortacardenas@gmail.com>
2025-05-21 13:00:11 -0400

    Styling noticias

--- src/components/social/noticias.tsx
+++ src/components/social/noticias.tsx
@@ -70,7 +70,7 @@ const Noticias: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 sm:p-6 lg:p-8 bg-slate-50 min-h-screen">
+    <div className="flex min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8 bg-[url('/textura1.jpg')] md:bg-[url('/noticias-home.jpg')] md:bg-contain md:bg-no-repeat md:bg-center">
       <header className="flex flex-col sm:flex-row justify-between items-center mb-8 pb-4 border-b border-slate-200">
         <div className="flex items-center mb-4 sm:mb-0">
           <Newspaper className="h-10 w-10 text-blue-600 mr-3" />
