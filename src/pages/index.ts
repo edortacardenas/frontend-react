b94083a1048e9de3878d5a2796464a1654bf4a76
Erick Dorta Cardenas <edortacardenas@gmail.com>
2025-05-22 12:22:04 -0400

    Rectyfing pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,13 +2,12 @@ import Home from "@/components/personal/home/home";
 import Login from "@/components/auth/login/login-form";
 import Register from "@/components/auth/register/register-form";
 import Dashboard from "@/components/personal/dashboard/dashboard";
-
+import Noticias from "@/components/social/noticias";
+import Config from "@/components/personal/config/config";
 import React from "react";
 
-export { Home, Login, Register, Dashboard }
+export { Home, Login, Register, Dashboard, Noticias, Config  }
 
-export const Config = React.lazy(() => import("../components/personal/config/config"));
 export const ForgotPassword = React.lazy(() => import("../components/auth/forgot-password/forgot-password"));
 export const ResetPassword = React.lazy(() => import("../components/auth/reset-password/reset-password"));
-export const Noticias = React.lazy(() => import("../components/social/noticias"));
 export const VerifyEmail = React.lazy(() => import("../components/auth/verify-email/verify-email"));
