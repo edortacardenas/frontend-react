d24be715c0aa2c44f2ee10ad885ca1fe691dd319
Erick Dorta Cardenas <edortacardenas@gmail.com>
2025-05-22 12:17:28 -0400

    Improving loading page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,13 +1,12 @@
 import Home from "@/components/personal/home/home";
 import Login from "@/components/auth/login/login-form";
 import Register from "@/components/auth/register/register-form";
+import Dashboard from "@/components/personal/dashboard/dashboard";
+
 import React from "react";
 
-export { Home, Login, Register }
+export { Home, Login, Register, Dashboard }
 
-//export const Login = React.lazy(() => import("../components/auth/login/login-form"));
-//export const Register = React.lazy(() => import("../components/auth/register/register-form"));
-export const Dashboard = React.lazy(() => import("../components/personal/dashboard/dashboard"));
 export const Config = React.lazy(() => import("../components/personal/config/config"));
 export const ForgotPassword = React.lazy(() => import("../components/auth/forgot-password/forgot-password"));
 export const ResetPassword = React.lazy(() => import("../components/auth/reset-password/reset-password"));
