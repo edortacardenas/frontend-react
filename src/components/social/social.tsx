6e433bc533bf501f8ab6f2e24dd56d35a547aece
Erick Dorta Cardenas <edortacardenas@gmail.com>
2025-07-21 12:11:53 -0400

    change url /auth/google

--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -7,12 +7,12 @@ const Social = () => {
   return (
     <div className="grid grid-cols-2 gap-3">
           <Button variant="ghost" className="w-full bg-blue-200" asChild>
-            <Link to={`${import.meta.env.VITE_BACKEND_URL}/api/auth/callback/google`} className="flex items-center justify-center gap-2">
+            <Link to={`${import.meta.env.VITE_BACKEND_URL}/api/auth/google`} className="flex items-center justify-center gap-2">
               <SiGoogle className="h-5 w-5" /> Google
             </Link>
           </Button>
           <Button variant="ghost" className="w-full bg-blue-200" asChild>
-            <Link to={`${import.meta.env.VITE_BACKEND_URL}/api/auth/callback/github`} className="flex items-center justify-center gap-2">
+            <Link to={`${import.meta.env.VITE_BACKEND_URL}/api/auth/github`} className="flex items-center justify-center gap-2">
               <SiGithub className="h-5 w-5 " /> GitHub
             </Link>
           </Button>
